dd2cb940e783f70c6bc754f1c7178eb9cf6cdcea
Norbert Koziana <koziana11norbert@gmail.com>
2024-11-16 17:19:49 +0100

    initial commit

---
+++ src/main/java/com/marafone/marafone/MarafoneApplication.java
@@ -0,0 +1,13 @@
+package com.marafone.marafone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarafoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarafoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marafone/marafone/MarafoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marafone.marafone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarafoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
