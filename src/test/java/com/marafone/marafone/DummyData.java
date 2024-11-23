920afcdc565c5331aa82ee724b489a327e215641
Norbert Koziana <koziana11norbert@gmail.com>
2024-11-23 21:22:15 +0100

    Added dummy data for testing

---
+++ src/test/java/com/marafone/marafone/DummyData.java
@@ -0,0 +1,71 @@
+package com.marafone.marafone;
+
+import com.marafone.marafone.game.model.*;
+import com.marafone.marafone.user.User;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DummyData {
+
+    public static User getUserA(){
+        return User.builder()
+                .id(1L)
+                .username("John")
+                .email("john@gmail.com")
+                .password("123")
+                .build();
+    }
+
+    public static Card getCardA(){
+        return new Card(1L, CardRank.FIVE, Suit.COINS);
+    }
+
+    public static GamePlayer getGamePlayerA(){
+        return GamePlayer.builder()
+                .id(1L)
+                .user(getUserA())
+                .team(Team.RED)
+                .points(17)
+                .ownedCards(
+                        new ArrayList<>(
+                            Arrays.asList(
+                                getCardA(),
+                                new Card(2L, CardRank.C, Suit.CUPS),
+                                new Card(3L, CardRank.A, Suit.CLUBS)
+                            )
+                        )
+                )
+                .build();
+    }
+
+    public static Action getActionA(){
+        return Action.builder()
+                .id(1L)
+                .player(getGamePlayerA())
+                .card(getCardA())
+                .timestamp(LocalDateTime.of(2024, 2, 27, 13, 2))
+                .build();
+    }
+
+    public static Round getRoundA(){
+        return Round.builder()
+                .actions(new ArrayList<>(Arrays.asList(getActionA())))
+                .trumpSuit(Suit.SWORDS)
+                .build();
+    }
+
+    public static Game getGameA(){
+        return Game.builder()
+                .createdAt(LocalDateTime.of(2024, 2, 27, 13, 2))
+                .startedAt(LocalDateTime.of(2024, 2, 27, 13, 3))
+                .playersList(new ArrayList<>(Arrays.asList(getGamePlayerA())))
+                .rounds(new ArrayList<>(Arrays.asList(getRoundA())))
+                .gameType(GameType.MARAFFA)
+                .owner(getGamePlayerA())
+                .currentPlayerTurn(new ArrayList<>(Arrays.asList(getGamePlayerA())).listIterator())
+                .build();
+    }
+
+}
