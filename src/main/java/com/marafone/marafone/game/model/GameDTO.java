2076df7e8784c8db719f1e99645739ae1a2748ef
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-12-27 14:02:13 +0100

    send long value as string to prevent values rounding

--- src/main/java/com/marafone/marafone/game/model/GameDTO.java
+++ src/main/java/com/marafone/marafone/game/model/GameDTO.java
@@ -1,5 +1,5 @@
 package com.marafone.marafone.game.model;
 
 
-public record GameDTO(Long gameId, String gameName, GameType gameType, Integer joinedPlayersAmount) {
+public record GameDTO(String gameId, String gameName, GameType gameType, Integer joinedPlayersAmount) {
 }
