b10ebb096a99301133c372e0275f2c96e1405c82
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-01-01 10:14:32 +0100

    errors catalog added

---
+++ src/main/java/com/marafone/marafone/errors/SelectCardErrorMessages.java
@@ -0,0 +1,19 @@
+package com.marafone.marafone.errors;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum SelectCardErrorMessages {
+    NOT_YOUR_TURN("Not your turn! %s is deciding their move. Get ready for your next turn."),
+    CARD_NOT_IN_HAND("The card you tried to play is not in your hand. Please select a valid card."),
+    TRUMP_SUIT_NOT_SELECTED("You cannot play a card until the trump suit is selected."),
+    INVALID_TRUMP_SUIT_PLAY("Selected card is not part of the trump suit. Please play a valid card from the trump suit, %s.");
+
+    private final String message;
+
+    public String formatMessage(Object... args) {
+        return String.format(message, args);
+    }
+}
