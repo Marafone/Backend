dee4db3be374ab5435a3949d8707345e720391c2
Norbert Koziana <koziana11norbert@gmail.com>
2025-01-03 23:01:20 +0100

    added sockjs

--- src/main/java/com/marafone/marafone/game/config/WebSocketConfiguration.java
+++ src/main/java/com/marafone/marafone/game/config/WebSocketConfiguration.java
@@ -16,7 +16,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/game").setAllowedOrigins(frontendUrl);
+        registry.addEndpoint("/game").setAllowedOrigins(frontendUrl).withSockJS();
     }
 
     @Override
