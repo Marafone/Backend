a16d806ccbde0efd3063d785c366dfc257a4a9be
Norbert Koziana <koziana11norbert@gmail.com>
2025-01-03 16:43:45 +0100

    new build

---
+++ target/classes/data.sql
@@ -0,0 +1,49 @@
+--INSERTS CARDS INTO DATABASE
+
+INSERT INTO card VALUES
+
+(1, 'THREE', 'SWORDS'),
+(2, 'TWO', 'SWORDS'),
+(3, 'A', 'SWORDS'),
+(4, 'K', 'SWORDS'),
+(5, 'C', 'SWORDS'),
+(6, 'J', 'SWORDS'),
+(7, 'SEVEN', 'SWORDS'),
+(8, 'SIX', 'SWORDS'),
+(9, 'FIVE', 'SWORDS'),
+(10, 'FOUR', 'SWORDS'),
+
+(11, 'THREE', 'CUPS'),
+(12, 'TWO', 'CUPS'),
+(13, 'A', 'CUPS'),
+(14, 'K', 'CUPS'),
+(15, 'C', 'CUPS'),
+(16, 'J', 'CUPS'),
+(17, 'SEVEN', 'CUPS'),
+(18, 'SIX', 'CUPS'),
+(19, 'FIVE', 'CUPS'),
+(20, 'FOUR', 'CUPS'),
+
+(21, 'THREE', 'COINS'),
+(22, 'TWO', 'COINS'),
+(23, 'A', 'COINS'),
+(24, 'K', 'COINS'),
+(25, 'C', 'COINS'),
+(26, 'J', 'COINS'),
+(27, 'SEVEN', 'COINS'),
+(28, 'SIX', 'COINS'),
+(29, 'FIVE', 'COINS'),
+(30, 'FOUR', 'COINS'),
+
+(31, 'THREE', 'CLUBS'),
+(32, 'TWO', 'CLUBS'),
+(33, 'A', 'CLUBS'),
+(34, 'K', 'CLUBS'),
+(35, 'C', 'CLUBS'),
+(36, 'J', 'CLUBS'),
+(37, 'SEVEN', 'CLUBS'),
+(38, 'SIX', 'CLUBS'),
+(39, 'FIVE', 'CLUBS'),
+(40, 'FOUR', 'CLUBS')
+
+ON CONFLICT DO NOTHING;
