1ecf3d2de3227fc003b2848e3f1a47528f959d73
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-01-04 11:39:58 +0100

    new events added to event's hierarchy

--- src/main/java/com/marafone/marafone/game/event/outgoing/OutEvent.java
+++ src/main/java/com/marafone/marafone/game/event/outgoing/OutEvent.java
@@ -4,7 +4,7 @@ public abstract sealed class OutEvent permits
         MyCardsState, NewRound, PlayersOrderState, PointState,
         TeamState, TrumpSuitState, TurnState, WinnerState,
         PlayerJoinedEvent, PlayerLeftEvent, GameStartedEvent,
-        ErrorEvent {
+        ErrorEvent, NextPlayerState, CallState {
 
     public final String eventType;
 
