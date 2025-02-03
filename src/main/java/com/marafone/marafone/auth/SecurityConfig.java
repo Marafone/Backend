60abf8134291d8c4c9ce33d38521c387ef025979
TomaszJarzabek15 <tomaszjarzabek015@gmail.com>
2025-02-03 22:42:08 +0100

    MAR-32 Updated logout to return 200 (OK)

--- src/main/java/com/marafone/marafone/auth/SecurityConfig.java
+++ src/main/java/com/marafone/marafone/auth/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -69,7 +70,8 @@ public class SecurityConfig {
                         .anyRequest().permitAll()
                 )
                 .logout((logout) -> logout
-                        .logoutUrl("/auth/logout").logoutSuccessUrl("/")
+                        .logoutUrl("/auth/logout")
+                        .logoutSuccessHandler((new HttpStatusReturningLogoutSuccessHandler()))
                 )
         ;
         return http.build();
