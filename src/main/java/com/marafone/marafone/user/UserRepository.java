44921fdd96e0cec2513da5ab1933f56a796078cc
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-02-14 12:23:52 +0100

    MAR-16 Create string constant to reuse order by clause. Unnecessary parts from query removed.

--- src/main/java/com/marafone/marafone/user/UserRepository.java
+++ src/main/java/com/marafone/marafone/user/UserRepository.java
@@ -12,57 +12,53 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-
-    @Query("""
-        SELECT new com.marafone.marafone.user.dto.UserRankingDTO(DENSE_RANK() OVER (
+    String ORDER_DESCENDING_BY_WIN_RATIO =
+            """
             ORDER BY
                 CASE WHEN (u.wins + u.losses) = 0 THEN 0
                 ELSE (u.wins * 1.0 / (u.wins + u.losses)) END
             DESC
-        ) AS position,
-        u.username,
-        u.wins,
-        u.losses)
-        FROM User u
-        """)
+            """;
+
+    Optional<User> findByUsername(String username);
+
+    @Query("SELECT new com.marafone.marafone.user.dto.UserRankingDTO(DENSE_RANK() OVER ("
+            + ORDER_DESCENDING_BY_WIN_RATIO + """
+            ) AS position,
+            u.username,
+            u.wins,
+            u.losses)
+            FROM User u
+            """)
     Page<UserRankingDTO> getTopUsers(Pageable pageable);
 
     @Query("""
-        WITH RankedUsers AS (
-            SELECT
-                DENSE_RANK() OVER (
-                    ORDER BY
-                        CASE WHEN (u.wins + u.losses) = 0 THEN 0
-                        ELSE (u.wins * 1.0 / (u.wins + u.losses)) END
-                    DESC
-                ) AS position,
-                u.username AS username,
-                u.wins AS wins,
-                u.losses AS losses
-            FROM User u
-        )
-        SELECT new com.marafone.marafone.user.dto.UserRankingDTO(ru.position, ru.username, ru.wins, ru.losses)
-        FROM RankedUsers ru
-        WHERE ru.username = :username
-       """)
+            WITH RankedUsers AS (
+                SELECT
+                    DENSE_RANK() OVER ("""
+                        + ORDER_DESCENDING_BY_WIN_RATIO + """
+                    ) AS position,
+                    u.username AS username,
+                    u.wins AS wins,
+                    u.losses AS losses
+                FROM User u
+            )
+            SELECT new com.marafone.marafone.user.dto.UserRankingDTO(ru.position, ru.username, ru.wins, ru.losses)
+            FROM RankedUsers ru
+            WHERE ru.username = :username
+            """)
     UserRankingDTO getTopUser(@Param("username") String username);
 
     @Query("""
-        WITH RankedUsers AS (
-            SELECT
-                u.username AS username,
-                u.wins AS wins,
-                u.losses AS losses,
-                ROW_NUMBER() OVER (
-                    ORDER BY
-                        CASE WHEN (u.wins + u.losses) = 0 THEN 0
-                        ELSE (u.wins * 1.0 / (u.wins + u.losses)) END
-                    DESC
-                ) AS position
-            FROM User u
-        )
-        SELECT position FROM RankedUsers WHERE username = :username
-        """)
+            WITH RankedUsers AS (
+                SELECT
+                    u.username AS username,
+                    ROW_NUMBER() OVER ("""
+                        + ORDER_DESCENDING_BY_WIN_RATIO + """
+                    ) AS position
+                FROM User u
+            )
+            SELECT position FROM RankedUsers WHERE username = :username
+            """)
     Optional<Integer> getUserRankingPosition(@Param("username") String username);
 }
