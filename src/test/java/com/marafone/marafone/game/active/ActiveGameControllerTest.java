a8a117796b03ef4090f254aaaa0aa3bedfa5b550
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-02-22 19:08:36 +0100

    MAR-43 Tests adjusted. Test to check game creation when user plays another game added.

--- src/test/java/com/marafone/marafone/game/active/ActiveGameControllerTest.java
+++ src/test/java/com/marafone/marafone/game/active/ActiveGameControllerTest.java
@@ -4,14 +4,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.marafone.marafone.game.ended.EndedGameService;
 import com.marafone.marafone.game.event.incoming.CreateGameRequest;
 import com.marafone.marafone.game.model.GameType;
+import com.marafone.marafone.user.User;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -50,13 +59,45 @@ class ActiveGameControllerTest {
     }
 
     @Test
-    @WithMockUser(username = "testUser" )
-    void createGame_WhenGameDoesNotExist_ShouldReturnOkResponseInfo() throws Exception {
+    void createGame_WhenUserInAnotherGame_ShouldReturnBadRequestInfo() throws Exception {
         // given
+        User mockedUser = Mockito.mock(User.class);
+        Mockito.when(mockedUser.getUsername()).thenReturn("testUser");
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(mockedUser, null, List.of());
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+
         CreateGameRequest createGameRequest =
                 new CreateGameRequest("game", GameType.MARAFFA, "", 21);
 
         Mockito.when(activeGameService.doesNotStartedGameAlreadyExist(Mockito.any())).thenReturn(false);
+        Mockito.when(activeGameService.getReconnectableGameForPlayer(Mockito.any())).thenReturn(Optional.of(1L));
+        String jsonRequest = new ObjectMapper().writeValueAsString(createGameRequest);
+
+        // when & then
+        mockMvc.perform(post("/game/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequest)
+                .with(csrf())) // to include csrf token for authentication
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createGame_WhenGameDoesNotExist_ShouldReturnOkResponseInfo() throws Exception {
+        // given
+        User mockedUser = Mockito.mock(User.class);
+        Mockito.when(mockedUser.getUsername()).thenReturn("testUser");
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(mockedUser, null, List.of());
+        context.setAuthentication(authentication);
+        SecurityContextHolder.setContext(context);
+
+        CreateGameRequest createGameRequest =
+                new CreateGameRequest("game", GameType.MARAFFA, "", 21);
+
+        Mockito.when(activeGameService.doesNotStartedGameAlreadyExist(Mockito.any())).thenReturn(false);
+        Mockito.when(activeGameService.getReconnectableGameForPlayer(Mockito.any())).thenReturn(Optional.empty());
         String jsonRequest = new ObjectMapper().writeValueAsString(createGameRequest);
 
         // when & then
