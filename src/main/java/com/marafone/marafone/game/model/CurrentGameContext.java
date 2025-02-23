bf635d771c777cab99e229b20b39ffe54197be54
Blizni13 <szymo@DESKTOP-V0MIKMF>
2025-02-23 13:12:30 +0100

    SelectCardException added

--- src/main/java/com/marafone/marafone/game/model/CurrentGameContext.java
+++
@@ -1,4 +0,0 @@
-package com.marafone.marafone.game.model;
-
-public record CurrentGameContext(Long gameId, GamePlayer currentPlayer, Round currentRound, Card selectedCard) {
-}
--- src/main/java/com/marafone/marafone/game/model/GameDTO.java
+++
@@ -1,5 +0,0 @@
-package com.marafone.marafone.game.model;
-
-
-public record GameDTO(String gameId, String gameName, GameType gameType, Integer joinedPlayersAmount) {
-}
